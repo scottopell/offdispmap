12c85c49ac21a51b42420621532f55d92193d536
Scott Opell <me@scottopell.com>
2024-06-16 09:06:15 -0400

    initial commit

---
+++ offdispmap/offdispmapApp.swift
@@ -0,0 +1,17 @@
+//
+//  offdispmapApp.swift
+//  offdispmap
+//
+//  Created by Scott Opell on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct offdispmapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
